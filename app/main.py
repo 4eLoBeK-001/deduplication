ad652aa7a2d07f9dafd34182f9b6c2ac7cc4f069
4eLoBeK-001 <kostantin.ef@gmail.com>
2026-02-13 17:38:35 +0500

    refactor: cleaned the code
    refactor: moved the subdomain to separate variable

--- app/main.py
+++ app/main.py
@@ -1,12 +1,20 @@
+import asyncio
+import os
+import httpx
+
+from dotenv import load_dotenv
 from pprint import pprint
 from fastapi import FastAPI, Request
-import httpx
 
 from app.services.helpers import (
     extract_phone_final, find_contacts_by_phone, clean_phone, 
     find_duplicate, get_contact_notes, link_lead_to_contact, 
     transfer_notes, update_original_contact
 )
+
+load_dotenv()
+
+SUBDOMAIN = os.getenv("AMOCRM_SUBDOMAIN")
 
 
 app = FastAPI(title='amoCRM')
@@ -15,7 +23,6 @@ app = FastAPI(title='amoCRM')
 @app.get('/')
 async def test(request: Request):
     return {"status": "ok"}
-
 
 
 @app.post('/webhook')
@@ -27,21 +34,26 @@ async def test_request(request: Request):
     
     contact_id = data.get('contacts[add][0][id]')
     original_phone = await extract_phone_final(data)
+    
+    if not original_phone:
+        return {'text': 'phone nt found'}
 
     original_phone = clean_phone(original_phone)
-    contact = await find_contacts_by_phone(original_phone, 'kostantinef')
+    found_contacts = await find_contacts_by_phone(original_phone, SUBDOMAIN)
     
     # Если у контакта есть сделки
     lead_id = 0
-    if contact[-1].get('_embedded').get('leads'):
-        lead_id = contact[-1].get('_embedded').get('leads')[0].get('id')
+    if found_contacts[-1].get('_embedded').get('leads'):
+        lead_id = found_contacts[-1].get('_embedded').get('leads')[0].get('id')
 
-    if len(contact) > 1:
-        original, duplicate = await find_duplicate(contact)
-        await update_original_contact(original, duplicate, 'kostantinef')
+    if len(found_contacts) > 1:
+        original, duplicate = await find_duplicate(found_contacts)
+
+        await asyncio.sleep(2)
+        await update_original_contact(original, duplicate, SUBDOMAIN)
         await link_lead_to_contact(lead_id, original.get('id'))
-        notes = await get_contact_notes(duplicate.get('id'), 'kostantinef')
-        await transfer_notes(notes, original.get('id'), 'kostantinef')
+        notes = await get_contact_notes(duplicate.get('id'), SUBDOMAIN)
+        await transfer_notes(notes, original.get('id'), SUBDOMAIN)
         return {'Успешно': 'Супер!'}
     
     return {"status": "ok"}
