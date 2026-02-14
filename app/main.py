508a654978c9fc7f49b82ad66f3a2757e10aa13d
4eLoBeK-001 <kostantin.ef@gmail.com>
2026-02-14 21:01:41 +0500

    added logging

--- app/main.py
+++ app/main.py
@@ -10,6 +10,7 @@ from app.services.helpers import (
     link_lead_to_contact, transfer_notes, update_original_contact
 )
 from app.services.utils import clean_phone, extract_phone_final
+from app.core.logger import logger
 
 load_dotenv()
 
@@ -26,43 +27,59 @@ async def test(request: Request):
 
 @app.post('/webhook')
 async def test_request(request: Request):
+    logger.info('Webhook received')
+   
+    try:
+        form_data = await request.form()
+        data = dict(form_data)
 
-    form_data = await request.form()
-    
-    data = dict(form_data)
-    
-    contact_id = data.get('contacts[add][0][id]')
-    original_phone = await extract_phone_final(data)
-    
-    if not original_phone:
-        return {'text': 'phone nt found'}
+        logger.info('Webhook data parsed')
+        
+        original_phone = await extract_phone_final(data)
 
-    original_phone = clean_phone(original_phone)
-    found_contacts = await find_contacts_by_phone(original_phone, SUBDOMAIN)
-    
-    # Если у контакта есть сделки
-    lead_id = 0
-    pprint(found_contacts)
-    if found_contacts[-1].get('_embedded').get('leads'):
-        lead_id = found_contacts[-1].get('_embedded').get('leads')[0].get('id')
+        if not original_phone:
+            logger.warning('Phone not found in webhook')
+            return {'text': 'phone nt found'}
+        
+        logger.info(f'Phone extracted: {original_phone}')
 
-    if len(found_contacts) > 1:
-        original, duplicate = await find_duplicate(found_contacts)
+        original_phone = clean_phone(original_phone)
+        found_contacts = await find_contacts_by_phone(original_phone, SUBDOMAIN)
 
-        # После того как поняли что есть оригинал, ждём 2 секунды
-        await asyncio.sleep(2)
+        logger.info(f'Contacts found: {len(found_contacts)}')
+        
+        # Если у контакта есть сделки
+        lead_id = 0
+        if found_contacts[-1].get('_embedded').get('leads'):
+            lead_id = found_contacts[-1].get('_embedded').get('leads')[0].get('id')
 
-        # Обновляем поля у оригинала
-        await update_original_contact(original, duplicate, SUBDOMAIN)
-        # Привязываем сделки из дубликата к оригиналу
-        await link_lead_to_contact(lead_id, original.get('id'))
+        if len(found_contacts) > 1:
+            logger.info('Duplicate detected')
+            original, duplicate = await find_duplicate(found_contacts)
 
-        # Переносим примечания
-        notes = await get_contact_notes(duplicate.get('id'), SUBDOMAIN)
-        await transfer_notes(notes, original.get('id'), SUBDOMAIN)
+            logger.info(
+                f'Original ID={original.get('id')} | Duplicate ID={duplicate.get('id')}'
+            )
 
-        print('WITH')
+            # После того как поняли что есть оригинал, ждём 2 секунды
+            await asyncio.sleep(2)
+
+            logger.info('Starting merge process')
+            # Обновляем поля у оригинала
+            await update_original_contact(original, duplicate, SUBDOMAIN)
+            logger.info('Merge completed')
+
+            # Привязываем сделки из дубликата к оригиналу
+            await link_lead_to_contact(lead_id, original.get('id'))
+
+            # Переносим примечания
+            notes = await get_contact_notes(duplicate.get('id'), SUBDOMAIN)
+            await transfer_notes(notes, original.get('id'), SUBDOMAIN)
+
+        
+        logger.info('Webhook processing finished')
         return {'status': 'ok'}
 
-    print('BEZ')
-    return {'status': 'ok'}
+    except Exception as e:
+        logger.exception('Error in webhook')
+        raise
