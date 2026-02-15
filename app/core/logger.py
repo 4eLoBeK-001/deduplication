495c1ea80450a99c28cc23bdd96d72313f843f4f
4eLoBeK-001 <kostantin.ef@gmail.com>
2026-02-15 18:23:21 +0500

    optimized logger file
    added logs to main.py

--- app/core/logger.py
+++ app/core/logger.py
@@ -4,23 +4,28 @@ import sys
 
 logger.remove()
 
-
-logger.add(sys.stdout, format="{time:HH:mm:ss} | {level} | {message}", level="INFO")
+fmt = '{time:HH:mm:ss} | {level} | {extra[request_id]} | {message}\n'
+default_fmt = '{time:HH:mm:ss} | {level} | {message}\n'
 
 logger.add(
-    "logs/short_errors.log",
-    format="{time:YYYY-MM-DD HH:mm:ss} | Ошибка в {function}:{line} | Сообщение: {message}",
-    level="ERROR",
-    filter=lambda record: record["level"].name == "ERROR", # Только ошибки
-    rotation="10 MB",
-    compression="zip"
+    sys.stdout, 
+    format=lambda record: fmt if 'request_id' in record['extra'] else default_fmt, 
+    level='INFO'
 )
 
 logger.add(
-    "logs/full_tracebacks.log",
-    level="ERROR",
+    'logs/main.log',
+    level='INFO', 
+    format=lambda record: fmt if 'request_id' in record['extra'] else default_fmt, 
+    rotation='50 MB', 
+    compression='zip'
+)
+
+logger.add(
+    'logs/full_tracebacks.log',
+    level='ERROR',
     backtrace=True,  # Показывает путь ошибки
     diagnose=True,   # Показывает значения переменных в момент падения
-    rotation="10 MB",
-    compression="zip"
+    rotation='10 MB',
+    compression='zip'
 )
--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 import asyncio
 import os
+import uuid
 
 from dotenv import load_dotenv
 from pprint import pprint
@@ -27,60 +28,62 @@ async def test(request: Request):
 
 @app.post('/webhook')
 async def test_request(request: Request):
-    logger.info('Webhook received')
-   
-    try:
-        form_data = await request.form()
-        data = dict(form_data)
+    request_id = str(uuid.uuid4())[:8]
+    
+    with logger.contextualize(request_id=request_id):
+        try:
+            logger.info('Webhook received')
+            form_data = await request.form()
+            data = dict(form_data)
 
-        logger.info('Webhook data parsed')
-        
-        original_phone = await extract_phone_final(data)
+            logger.info('Webhook data parsed')
+            
+            original_phone = await extract_phone_final(data)
 
-        if not original_phone:
-            logger.warning('Phone not found in webhook')
-            return {'text': 'phone nt found'}
-        
-        logger.info(f'Phone extracted: {original_phone}')
+            if not original_phone:
+                logger.warning('Phone not found in webhook')
+                return {'text': 'phone nt found'}
+            
+            logger.info(f'Phone extracted: {original_phone}')
 
-        original_phone = clean_phone(original_phone)
-        found_contacts = await find_contacts_by_phone(original_phone, SUBDOMAIN)
+            original_phone = clean_phone(original_phone)
+            found_contacts = await find_contacts_by_phone(original_phone, SUBDOMAIN)
 
-        logger.info(f'Contacts found: {len(found_contacts)}')
-        
-        # Если у контакта есть сделки
-        lead_id = 0
-        if found_contacts[-1].get('_embedded').get('leads'):
-            lead_id = found_contacts[-1].get('_embedded').get('leads')[0].get('id')
+            logger.info(f'Contacts found: {len(found_contacts)}')
+            
+            # Если у контакта есть сделки
+            lead_id = 0
+            if found_contacts[-1].get('_embedded').get('leads'):
+                lead_id = found_contacts[-1].get('_embedded').get('leads')[0].get('id')
 
-        if len(found_contacts) > 1:
-            logger.info('Duplicate detected')
-            original, duplicate = await find_duplicate(found_contacts)
+            if len(found_contacts) > 1:
+                logger.info('Duplicate detected')
+                original, duplicate = await find_duplicate(found_contacts)
 
-            logger.info(
-                f'Original ID={original.get('id')} | Duplicate ID={duplicate.get('id')}'
-            )
+                logger.info(
+                    f'Original ID={original.get('id')} | Duplicate ID={duplicate.get('id')}'
+                )
 
-            # После того как поняли что есть оригинал, ждём 2 секунды
-            await asyncio.sleep(2)
+                # После того как поняли что есть оригинал, ждём 2 секунды
+                await asyncio.sleep(2)
 
-            logger.info('Starting merge process')
-            # Обновляем поля у оригинала
-            await update_original_contact(original, duplicate, SUBDOMAIN)
-            logger.info('Merge completed')
+                logger.info('Starting merge process')
+                # Обновляем поля у оригинала
+                await update_original_contact(original, duplicate, SUBDOMAIN)
+                logger.info('Merge completed')
 
-            # Привязываем сделки из дубликата к оригиналу
-            await link_lead_to_contact(lead_id, original.get('id'))
+                # Привязываем сделки из дубликата к оригиналу
+                await link_lead_to_contact(lead_id, original.get('id'))
 
-            # Переносим примечания
-            notes = await get_contact_notes(duplicate.get('id'), SUBDOMAIN)
-            await transfer_notes(notes, original.get('id'), SUBDOMAIN)
+                # Переносим примечания
+                notes = await get_contact_notes(duplicate.get('id'), SUBDOMAIN)
+                await transfer_notes(notes, original.get('id'), SUBDOMAIN)
 
-        
-        logger.info('Webhook processing finished')
-        return {'status': 'ok'}
+            
+            logger.info('Webhook processing finished')
+            return {'status': 'ok'}
 
-    except Exception as e:
-        params = f'Phone: {original_phone}, lead_id: {lead_id}'
-        logger.error(f'Ошибка при обработке вебхука. Параметры: {params}. Ошибка: {e}')
-        raise
+        except Exception as e:
+            params = f'Phone: {original_phone}, lead_id: {lead_id}'
+            logger.error(f'Ошибка при обработке вебхука. Параметры: {params}. Ошибка: {e}')
+            raise
